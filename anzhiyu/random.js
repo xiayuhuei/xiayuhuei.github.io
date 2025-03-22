f791c3ea42982875c58c8113a3a5dbdfa7bc369e
xiaoyuhu <2671959074@qq.com>
2025-03-22 10:22:05 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/制作方向/","/cdds/","/hfgx/","/zbhf/"];function toRandomPost(){
+var posts=["/cdds/","2025/03/21/制作方向/","/hfgx/","/zbhf/","2025/03/22/素材购买/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
