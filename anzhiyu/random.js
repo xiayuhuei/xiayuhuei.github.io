03e58e8d66ecba868c315b78bc87f2180691ce54
xiaoyuhu <2671959074@qq.com>
2025-03-25 01:02:26 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/wz/SMR-Helpers/","/wz/cdds/","/wz/制作进度/","/wz/zbhf/","/wz/恢复更新/","/wz/动画素材/"];function toRandomPost(){
+var posts=["/wz/cdds/","/wz/SMR-Helpers/","/wz/恢复更新/","/wz/制作进度/","/wz/zbhf/","/wz/动画素材/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
