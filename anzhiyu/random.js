f16143fda277fb7cbacf0a8a5b801be2de032e2c
xiaoyuhu <2671959074@qq.com>
2025-03-26 20:59:16 +0800

    Add files via upload

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/wz/2025-03-24/","/wz/cdds/","/wz/SMR-Helpers/","/wz/制作进度/","/wz/恢复更新/","/wz/zbhf/","/wz/动画素材/"];function toRandomPost(){
+var posts=["/wz/2025-03-24/","/wz/2025-03-25/","/wz/2025-03-26/","/wz/cdds/","/wz/制作进度/","/wz/恢复更新/","/wz/zbhf/","/wz/SMR-Helpers/","/wz/动画素材/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
