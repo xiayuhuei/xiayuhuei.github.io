c3966c7bbf6ab9372b2b97b84ec3b64c1793efd4
xiaoyuhu <2671959074@qq.com>
2025-03-26 21:28:26 +0800

    Delete anzhiyu directory

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["/wz/2025-03-24/","/wz/cdds/","/wz/SMR-Helpers/","/wz/制作进度/","/wz/恢复更新/","/wz/zbhf/","/wz/动画素材/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
