642c10a574fd0dd90c974405f35c5a58b01927ca
xiaoyuhu <2671959074@qq.com>
2025-04-10 04:41:06 +0800

    Delete anzhiyu directory

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["/wz/SMR-Helpers/","/wz/cdds/","/wz/AN插件/","/wz/我用幻术控制全世界/","/wz/动画素材/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
